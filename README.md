b07cc67f21f456c3d0eb61d736575a74ed138e24
DanielIozia <danieliozia.di@gmail.com>
2024-03-15 20:24:37 +0100

    sistemato apostrofo, bottone closeDescription e caricamento

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Bookeria
+Questo documento fornisce una panoramica del sito web **Bookeria** che utilizza le API di OpenLibrary per recuperare informazioni sui libri e visualizzarle agli utenti. Il sito consente agli utenti di cercare libri per genere e di visualizzare dettagli come titolo, autore e descrizione.
+
+## Funzionalità Principali
+
+- **Ricerca Libri per Genere**: Gli utenti possono selezionare un genere dalla lista e visualizzare i libri corrispondenti.
+- **Visualizzazione Titolo e Autori**: Ogni libro mostra il suo titolo e gli autori associati.
+- **Visualizzazione Descrizione**: È disponibile un pulsante "Show Description" per ogni libro che permette agli utenti di visualizzare una descrizione dettagliata del libro selezionato.
+- **Gestione degli Apostrofi**: Il sito gestisce correttamente gli apostrofi nei titoli dei libri per evitare errori di formattazione.
+- **Indicatore di Caricamento**: È stato aggiunto un indicatore visivo per mostrare quando la ricerca dei libri è in corso.
+
+
+## Utilizzo delle Funzioni
+
+- `searchBooks(event)`: Questa funzione viene attivata quando un utente seleziona un genere dalla lista. Utilizza l'API di OpenLibrary per recuperare i libri corrispondenti al genere scelto e li visualizza.
+- `addBackslashBeforeApostrophe(array)` e `there_is_apostrophe(array)`: Queste funzioni gestiscono gli apostrofi nei titoli dei libri per evitare errori di formattazione.
+- `showDescription(bookKey, authorName)`: Viene chiamata quando un utente clicca sul pulsante "Show Description". Utilizza l'API di OpenLibrary per ottenere e visualizzare una descrizione dettagliata del libro selezionato.
+- `closeDescription(bookKey, title, author)`: Viene chiamata quando un utente chiude la descrizione di un libro. Pulisce il contenuto precedente e ripristina il titolo e il pulsante "Show Description".
+
+## Contatti
+
+Per domande, suggerimenti o segnalazioni di bug, non esitare a contattarmi all'indirizzo email [danieliozia.di@gmail.com](mailto:danieliozia.di@gmail.com).
--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,16 +1,22 @@
+document.getElementById('loadingSpinner').style.display = 'none';
+
 function searchBooks(event) {
     event.preventDefault();
+
+    document.getElementById('loadingSpinner').style.display = 'block';
+
+
     const genere = document.getElementById('genere').value;
     fetch(`https://openlibrary.org/subjects/${genere}.json`)
         .then(response => response.json())
         .then(data => {
+            document.getElementById('loadingSpinner').style.display = 'none';
             const book = data.works;
             const booksList = document.getElementById('booksList');
             booksList.innerHTML = ''; // Pulizia della lista dei libri
             book.forEach(book => {
                 const title = book.title ? book.title : 'Title not available';
                 const authors = book.authors ? book.authors.map(author => author.name).join(', ') : 'Author not available';
-
 
                    //IMMAGINE
                 let coverId = 'nophoto'; // Impostiamo un valore predefinito per l'ID della copertina
@@ -34,8 +40,13 @@ function searchBooks(event) {
                 booksList.appendChild(bookElement);
             });
         })
-        .catch(error => console.error('Error fetching books:', error));
+        .catch(error => {
+            console.error('Error fetching books:', error);
+            // Assicurati di nascondere anche l'icona di caricamento in caso di errore
+            document.getElementById('loadingSpinner').style.display = 'none';
+        });
 }
+
 
 function addBackslashBeforeApostrophe(array) {
 
@@ -68,14 +79,18 @@ function showDescription(bookKey, authorName) {
         .then(response => response.json())
         .then(data => {
             let title = data.title ? data.title : 'Title not available';
+            console.log(data)
 
 
-            
+         
             //alcuni sono oggetti, mentre altri no (boo)
-            //const description = (data.description.value != undefined && data.description.value != null) ? data.description.value : data.description;
-            const description = data.description;
-            const cardBody = document.querySelector(`[data-book-key="${bookKey}"] .card-body`);
+            let description = (data?.description) ?  data.description : "Description not available";
 
+            if(description!="Description not available" && description?.value)
+                description=description.value
+           // const description = data.description;
+            const cardBody = document.querySelector(`[data-book-key="${bookKey}"] .card-body`);
+            
             //metto il "\" per evitare che ci siano errori (si chiudono le virgolette)
             if (there_is_apostrophe(title)) {
                 console.log("Titolo prima: ", title);
@@ -84,7 +99,7 @@ function showDescription(bookKey, authorName) {
             }
 
             cardBody.innerHTML = `
-                <h5 class="card-title text-center">${title}</h5>
+                <h5 class="card-title text-center">${title.replace("\\'", "'")}</h5>
                 <p class="card-text">${description}</p>
                 <!-- <p class="card-text">Author: ${authorName}</p> -->
                 <button type="button" class="btn-close" aria-label="Close" onclick="closeDescription('${bookKey}','${title}','${authorName}')"></button>
